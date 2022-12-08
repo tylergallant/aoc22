6af8ce80af6b7deae87b71dbd012f381c5a939ad
Tyler Gallant <tylergallant@live.com>
2022-12-07 23:38:45 -0500

    day1

---
+++ src/day1.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Control.Monad
+import Data.Bifunctor
+import Data.List
+import System.IO
+
+nonEmpty :: String -> String -> Bool
+nonEmpty s1 s2 = not $ any null [s1, s2]
+
+readAndSum :: [String] -> Int
+readAndSum = sum . map read
+
+sumElves :: String -> [Int]
+sumElves = map readAndSum . filter (/= [""]) . groupBy nonEmpty . lines
+
+solveA :: String -> String
+solveA = show . maximum . sumElves
+
+solveB :: String -> String
+solveB = show . sum . take 3 .  sortBy (flip compare) . sumElves
+
+main :: IO ()
+main = interact $ \i -> solveA i ++ "\n" ++ solveB i
+
