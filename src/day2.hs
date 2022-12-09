d7b8778eef5c5ca993cf46f7b82693005cc168a4
Tyler Gallant <tylergallant@live.com>
2022-12-09 00:49:34 -0500

    Day 2B

--- src/day2.hs
+++ src/day2.hs
@@ -6,18 +6,28 @@ import Text.ParserCombinators.ReadP
 
 data Shape = Rock | Paper | Scissors deriving (Eq, Enum)
 
-data Outcome = Win | Lose | Draw
+data Outcome = Win | Lose | Draw deriving Enum
 
-shapeToBeat :: Shape -> Shape
-shapeToBeat Rock = Paper
-shapeToBeat Paper = Scissors
-shapeToBeat Scissors = Rock
+shapeToWin :: Shape -> Shape
+shapeToWin Rock = Paper
+shapeToWin Paper = Scissors
+shapeToWin Scissors = Rock
+
+shapeToLose :: Shape -> Shape
+shapeToLose Rock = Scissors
+shapeToLose Paper = Rock
+shapeToLose Scissors = Paper
 
 roundOutcome :: Shape -> Shape -> Outcome
 roundOutcome opponent player
   | player == opponent = Draw
-  | player == shapeToBeat opponent = Win
+  | player == shapeToWin opponent = Win
   | otherwise = Lose
+
+shapeForOutcome :: Outcome -> Shape -> Shape
+shapeForOutcome Win = shapeToWin
+shapeForOutcome Lose = shapeToLose
+shapeForOutcome Draw = id
 
 scoreShape :: Shape -> Int
 scoreShape Rock = 1
@@ -33,24 +43,41 @@ scoreRound :: (Shape, Shape) -> Int
 scoreRound (opponent, player) = scoreShape player + scoreOutcome outcome
   where outcome = roundOutcome opponent player
 
-parseShape :: [Char] -> ReadP Shape
-parseShape = asum . zipWith (<$) [Rock ..] . map char
+scoreDesiredOutcome :: (Shape, Outcome) -> Int
+scoreDesiredOutcome (opponent, outcome) = scoreOutcome outcome + shapeScore
+  where shapeScore = scoreShape $ shapeForOutcome outcome opponent
+
+parseCharsAs :: [a] -> String -> ReadP a
+parseCharsAs values = asum . zipWith (<$) values . map char
+
+parseShape :: String -> ReadP Shape
+parseShape = parseCharsAs [Rock ..]
 
 parseOpponentShape :: ReadP Shape
-parseOpponentShape =  parseShape ['A', 'B', 'C']
+parseOpponentShape =  parseShape "ABC"
 
 parsePlayerShape :: ReadP Shape
-parsePlayerShape = parseShape ['X', 'Y', 'Z']
+parsePlayerShape = parseShape "XYZ"
 
-parseRound :: ReadP (Shape, Shape)
-parseRound = (,) <$> parseOpponentShape <*> (skipSpaces *> parsePlayerShape)
+parseOutcome :: ReadP Outcome
+parseOutcome = parseCharsAs [Win ..] "ZXY"
+
+parseRoundShapes :: ReadP (Shape, Shape)
+parseRoundShapes = (,) <$> parseOpponentShape <*> (skipSpaces *> parsePlayerShape)
+
+parseShapeOutcome :: ReadP (Shape, Outcome)
+parseShapeOutcome = (,) <$> parseOpponentShape <*> (skipSpaces *> parseOutcome)
+
+solve :: (a -> Int) -> ReadP a -> String -> String
+solve score p = show . sum . map score . catMaybes . map parse . lines
+  where parse = fmap fst . listToMaybe . filter (null.snd) . readP_to_S p
 
 solveA :: String -> String
-solveA = show . sum . map scoreRound . catMaybes . map parse . lines
-  where
-    fullParse = null . snd
-    parse = fmap fst . listToMaybe . filter fullParse . readP_to_S parseRound
+solveA = solve scoreRound parseRoundShapes
+
+solveB :: String -> String
+solveB = solve scoreDesiredOutcome parseShapeOutcome
 
 main :: IO ()
-main = interact solveA
+main = interact $ \i -> solveA i ++ "\n" ++ solveB i
 
