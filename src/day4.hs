9adfb0d0abaff011005ee4dd1b1c50e24fa67f4f
Tyler Gallant <tylergallant@live.com>
2022-12-21 17:08:16 -0500

    Day 4B

--- src/day4.hs
+++ src/day4.hs
@@ -5,9 +5,16 @@ import Text.ParserCombinators.ReadP
 
 type Range = (Int, Int)
 
+contains :: Range -> Int -> Bool
+contains (s, e) n = s <= n && n <= e
+
 fullOverlap :: (Range, Range) -> Bool
 fullOverlap (r1, r2) = firstContainsSecond r1 r2 || firstContainsSecond r2 r1
-  where firstContainsSecond (s1, e1) (s2, e2) = s1 <= s2 && e1 >= e2
+  where firstContainsSecond r (s, e) = contains r s && contains r e
+
+partialOverlap :: (Range, Range) -> Bool
+partialOverlap (r1, r2) = firstOverlapsSecond r1 r2 || firstOverlapsSecond r2 r1
+  where firstOverlapsSecond r (s, e) = contains r s || contains r e
 
 pRange :: ReadP Range
 pRange = (,) <$> pInt <*> (char '-' *> pInt)
@@ -19,8 +26,12 @@ pPair = (,) <$> pRange <*> (char ',' *> pRange)
 parse :: ReadP a -> String -> Maybe a
 parse p = listToMaybe . map fst . filter (null . snd) . readP_to_S p
 
-solve :: String -> String
-solve = show . length . filter fullOverlap . catMaybes . map (parse pPair) . lines
+solveA :: String -> String
+solveA = show . length . filter fullOverlap . catMaybes . map (parse pPair) . lines
+
+solveB :: String -> String
+solveB = show . length . filter partialOverlap . catMaybes . map (parse pPair) . lines
 
 main :: IO ()
-main = interact solve
+main = interact $ \i -> solveA i ++ "\n" ++ solveB i
+
